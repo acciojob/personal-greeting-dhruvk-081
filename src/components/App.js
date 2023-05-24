4d9ba057ae36f4f1d7bda5ec0f57b4c8875e02d8
dhruvk-081 <dhruvk081@gmail.com>
2023-05-24 17:44:31 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+ 
+  const handleChange = (e)=>{
+    setName(e.target.value)
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <label htmlFor="">Enter Your Name</label><br />
+        <input type="text" value={name} onChange={handleChange} /><br />
+        <p>{`Hello ${name}!`}</p>
     </div>
+
   )
 }
 
